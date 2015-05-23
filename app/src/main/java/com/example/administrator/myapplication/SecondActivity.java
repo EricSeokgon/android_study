e5c87c7fe43d6a8e2f475cd1a8cf4a7074e77bbc
hadesleee <leesk55@gmail.com>
2015-05-24 01:22:42 +0900

    액티비티 add

--- app/src/main/java/com/example/administrator/myapplication/SecondActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/SecondActivity.java
@@ -1,30 +1,28 @@
 package com.example.administrator.myapplication;
 
 import android.app.*;
-import android.content.*;
 import android.os.*;
 import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.*;
 
-public class MainActivity extends Activity {
+public class SecondActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_second);
 		
-		Button btn = (Button) findViewById(R.id.bt_01);
+		Button btn = (Button) findViewById(R.id.bt_02);
 		btn.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				Intent i = new Intent(MainActivity.this, SecondActivity.class);
-				startActivity(i);
+				finish();
 			}
 		});
-		
 	}
-
+	
 }
